57ecfcf4d87a611bce8f2e977af3c35bfc94c87d
Vitor Monteiro <mluizvitor@gmail.com>
2022-04-05 17:20:33 -0300

    💄 Updated Theme Including Android 12 Switch style

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -7,7 +7,6 @@ export const theme = createTheme({
     button: {
       fontWeight: 600,
     },
-    
   },
   palette: {
     mode: 'dark',
@@ -18,81 +17,116 @@ export const theme = createTheme({
       main: '#142F55',
     },
     success: {
-      main: '#0df287'
+      main: '#0df287',
     },
     warning: {
-      main: '#FEBD34'
+      main: '#FEBD34',
     },
     error: {
-      main: '#FE7060'
+      main: '#fe8779',
     },
     background: {
       default: '#191A1C',
-      paper: '#1A2027'
+      paper: '#1A2027',
     },
   },
   components: {
+    MuiCssBaseline: {
+      styleOverrides: `
+        @media (max-width: 600px) {
+          html { 
+            font-size: 87.5%
+          }
+        }
+      `,
+    },
     MuiButton: {
       styleOverrides: {
         root: {
+          paddingLeft: 12,
+          paddingRight: 12,
           borderRadius: 8,
+          borderWidth: 2,
+          ':hover': {
+            borderWidth: 2,
+          },
         },
-      }
+      },
     },
     MuiFab: {
       styleOverrides: {
         root: {
-          borderRadius: 16
+          borderRadius: 16,
         },
         extended: {
-          paddingRight: 24
-        }
-      }
+          paddingRight: 24,
+        },
+      },
     },
     MuiSvgIcon: {
       styleOverrides:{
         root: {
           width: 24,
-          height: 24
-        }
-      }
+          height: 24,
+        },
+      },
     },
     MuiDialog: {
       styleOverrides: {
         paper: {
-          borderRadius: 16
-        }
-      }
+          borderRadius: 16,
+        },
+      },
     },
     MuiDialogTitle: {
       styleOverrides: {
         root: {
           fontWeight: 600,
-        }
-      }
+        },
+      },
     },
     MuiOutlinedInput: {
       styleOverrides: {
         root: {
           borderRadius: 8,
           borderWidth: 2,
-        }
-      }
+        },
+        notchedOutline: {
+          borderWidth: 2,
+        },
+      },
     },
     MuiTooltip: {
       styleOverrides: {
         tooltip: {
-          fontSize: '0.85rem'
-        }
-      }
+          fontSize: '0.85rem',
+        },
+      },
     },
     MuiTableCell: {
       styleOverrides: {
         head: {
           fontSize: 16,
           fontWeight: 600,
-        }
-      }
-    }
-  }
+        },
+      },
+    },
+    MuiSwitch: {
+      styleOverrides: {
+        root: {
+          padding: 8,
+        },
+        track: {
+          borderRadius: 22 / 2,
+          opacity: 0.2,
+        },
+        thumb: {
+          boxShadow: 'none',
+          width: 16,
+          height: 16,
+          margin: 2,
+        },
+      },
+    },
+  },
 });
